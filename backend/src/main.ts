bac96703c910aabf91fe118ad32d120050c32b8c
Hugo Mancilla <h.mancilla@lifeware.cl>
2021-03-14 20:11:14 -0300

    agregada validación carpeta uploads

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,9 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { existsSync, mkdirSync } from 'fs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  if (!existsSync('./uploads')) {
+      mkdirSync('./uploads');
+      mkdirSync('./uploads/usuarios');
+      mkdirSync('./uploads/usuarios/cedula_identidad');
+      mkdirSync('./uploads/usuarios/licencias');
+  }
   await app.listen(8080);
 }
 bootstrap();
