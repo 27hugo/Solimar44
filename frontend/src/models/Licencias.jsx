40e6099a5bef0e39786ca5a88a253b13873c0149
Hugo Mancilla <h.mancilla@lifeware.cl>
2021-03-24 01:04:18 -0300

    agregados modelos

---
+++ frontend/src/models/Licencias.jsx
@@ -0,0 +1,9 @@
+export default class Licencias{
+    constructor(lic_id, lic_emision, lic_vencimiento, lic_frente, lic_reverso){
+        this.lic_id = lic_id;
+        this.lic_emision = lic_emision;
+        this.lic_vencimiento = lic_vencimiento;
+        this.lic_frente = lic_frente;
+        this.lic_reverso = lic_reverso;
+    }
+}
